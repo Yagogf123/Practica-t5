7a332d7e93dcda66e33227dc47bdad16130b7d86
BENJAMIN RISKE EBERHARDT <benrisebe@alu.edu.gva.es>
2022-11-26 21:12:37 +0100

    Segunda Version, añado codigo incial

--- Practica t5/Form1.Designer.cs
+++ Practica t5/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1_Click = new System.Windows.Forms.Button();
+            this.cbUrgente = new System.Windows.Forms.CheckBox();
+            this.txtTelegrama = new System.Windows.Forms.RichTextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1_Click
+            // 
+            this.button1_Click.Location = new System.Drawing.Point(398, 255);
+            this.button1_Click.Name = "button1_Click";
+            this.button1_Click.Size = new System.Drawing.Size(138, 77);
+            this.button1_Click.TabIndex = 0;
+            this.button1_Click.Text = "Coste";
+            this.button1_Click.UseVisualStyleBackColor = true;
+            this.button1_Click.Click += new System.EventHandler(this.button1_Click_Click);
+            // 
+            // cbUrgente
+            // 
+            this.cbUrgente.AutoSize = true;
+            this.cbUrgente.Location = new System.Drawing.Point(99, 255);
+            this.cbUrgente.Name = "cbUrgente";
+            this.cbUrgente.Size = new System.Drawing.Size(102, 24);
+            this.cbUrgente.TabIndex = 1;
+            this.cbUrgente.Text = "Urgente?";
+            this.cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(99, 38);
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(375, 130);
+            this.txtTelegrama.TabIndex = 2;
+            this.txtTelegrama.Text = "";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(99, 317);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 26);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.cbUrgente);
+            this.Controls.Add(this.button1_Click);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1_Click;
+        private System.Windows.Forms.CheckBox cbUrgente;
+        private System.Windows.Forms.RichTextBox txtTelegrama;
+        private System.Windows.Forms.TextBox txtPrecio;
     }
 }
 
